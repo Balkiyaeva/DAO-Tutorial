685851283d77d1b081f03de994faca061d18f040
balkiyaeva <amil.balkiyaeva@gmail.com>
2022-10-18 01:32:43 +0600

    Initial commit

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+    "0x2aCD8C5b6592CFAD3a5b7b769E1E185085Ba66F2";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
